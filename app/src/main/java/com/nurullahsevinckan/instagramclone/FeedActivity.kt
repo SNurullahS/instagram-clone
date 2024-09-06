a864fad3dece211905978a139fece549a0174bb7
SNurullahS <nurullah.sevinckan91@gmail.com>
2024-09-06 21:07:09 +0300

    XML UI template and implementation of button click are done

--- app/src/main/java/com/nurullahsevinckan/instagramclone/FeedActivity.kt
+++ app/src/main/java/com/nurullahsevinckan/instagramclone/FeedActivity.kt
@@ -1,6 +1,7 @@
 package com.nurullahsevinckan.instagramclone
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,5 +23,11 @@ class FeedActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun signInButtonClick(view: View){
+
+    }
+    fun signUpButtonClick(view: View){
+
     }
 }
--- app/src/main/java/com/nurullahsevinckan/instagramclone/PostActivity.kt
+++ app/src/main/java/com/nurullahsevinckan/instagramclone/PostActivity.kt
@@ -1,6 +1,7 @@
 package com.nurullahsevinckan.instagramclone
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,5 +23,11 @@ class PostActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun selectImageClick(view: View){
+
+    }
+    fun publishButtonClick(view: View){
+
     }
 }
