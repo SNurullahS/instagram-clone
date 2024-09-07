fb031d284db8119b667779e6946343c6fc5c4322
SNurullahS <nurullah.sevinckan91@gmail.com>
2024-09-07 19:05:03 +0300

    Sign out, Add post menu bar added

--- app/src/main/java/com/nurullahsevinckan/instagramclone/MainActivity.kt
+++ app/src/main/java/com/nurullahsevinckan/instagramclone/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.nurullahsevinckan.instagramclone
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.nurullahsevinckan.instagramclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private lateinit var userAuth :FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -22,5 +28,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        userAuth = Firebase.auth
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        var menuInfalater = menuInflater
+        menuInfalater.inflate(R.menu.insta_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.add_post){
+            //Add post
+            var intent = Intent(this@MainActivity,PostActivity::class.java)
+            startActivity(intent)
+
+        }else if(item.itemId == R.id.logout){
+            // logout
+            userAuth.signOut()
+            val intent = Intent(this@MainActivity,FeedActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
