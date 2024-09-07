7a1c8b4613a3d90533fd5212fcc1b2d8a77b3f53
SNurullahS <nurullah.sevinckan91@gmail.com>
2024-09-07 17:25:48 +0300

    Sign in and Sign up implementation with firebase added

--- app/src/main/java/com/nurullahsevinckan/instagramclone/FeedActivity.kt
+++ app/src/main/java/com/nurullahsevinckan/instagramclone/FeedActivity.kt
@@ -1,16 +1,26 @@
 package com.nurullahsevinckan.instagramclone
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.nurullahsevinckan.instagramclone.databinding.ActivityFeedBinding
 import com.nurullahsevinckan.instagramclone.databinding.ActivityPostBinding
+import org.checkerframework.common.returnsreceiver.qual.This
 
 class FeedActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFeedBinding
+    private lateinit var auth: FirebaseAuth //Need for authentication
+    private lateinit var userMail : String
+    private lateinit var userPassword :String
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +33,59 @@ class FeedActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        userMail = ""
+        userPassword = ""
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        //If current user is exist, directly go to main menu of app
+        val currentUser =auth.currentUser
+        if (currentUser != null){
+            val intent = Intent(this@FeedActivity,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
     fun signInButtonClick(view: View){
 
+        userMail = binding.mailText.text.toString()
+        userPassword = binding.passwordText.text.toString()
+        if(userMail.isEmpty() || userPassword.isEmpty()) {
+            Toast.makeText(this,"Enter Information Correctly",Toast.LENGTH_LONG).show()
+        }else{
+            auth.signInWithEmailAndPassword(userMail, userPassword).addOnSuccessListener{task ->
+                //success
+                val intent = Intent(this@FeedActivity,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener{task ->
+                //fail
+                Toast.makeText(this,task.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+
+        }
     }
     fun signUpButtonClick(view: View){
 
+        userMail = binding.mailText.text.toString()
+        userPassword = binding.passwordText.text.toString()
+        if(userMail.isEmpty() || userPassword.isEmpty()) {
+            Toast.makeText(this,"Enter Information Correctly",Toast.LENGTH_LONG).show()
+        }else{
+            auth.createUserWithEmailAndPassword(userMail, userPassword).addOnSuccessListener{task ->
+                //success
+                val intent = Intent(this@FeedActivity,MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }.addOnFailureListener{task ->
+                //fail
+                Toast.makeText(this,task.localizedMessage,Toast.LENGTH_LONG).show()
+            }
+
+        }
+
     }
+
 }
