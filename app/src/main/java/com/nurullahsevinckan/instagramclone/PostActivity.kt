4f8dc61730eb289c758b82185ddf4355c56f5b5c
SNurullahS <nurullah.sevinckan91@gmail.com>
2024-09-08 16:41:43 +0300

    Permission and image select actions added

--- app/src/main/java/com/nurullahsevinckan/instagramclone/PostActivity.kt
+++ app/src/main/java/com/nurullahsevinckan/instagramclone/PostActivity.kt
@@ -1,16 +1,31 @@
 package com.nurullahsevinckan.instagramclone
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.MediaStore.Images.Media
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 import com.nurullahsevinckan.instagramclone.databinding.ActivityMainBinding
 import com.nurullahsevinckan.instagramclone.databinding.ActivityPostBinding
 
 class PostActivity : AppCompatActivity() {
     private lateinit var binding: ActivityPostBinding
+    private lateinit var activityResultLauncger : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
+    private var selectedPicture : Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +38,58 @@ class PostActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        registerLauncher()
     }
     fun selectImageClick(view: View){
+    if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+    {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+            Snackbar.make(view,"Permission Needed For Gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission"){
+                //request permission
+                permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }.show()
+        }else{
+            // request permission
+            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+        }
 
+    }else{
+        //Intent gallery
+        val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        // Start activity for result
+        activityResultLauncger.launch(intentToGallery)
+
+    }
     }
     fun publishButtonClick(view: View){
 
+    }
+    private fun registerLauncher(){
+        activityResultLauncger = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){ result ->
+            if(result.resultCode == RESULT_OK){
+                var activityResultUri = result.data
+                if(activityResultUri != null) {
+                    selectedPicture = activityResultUri.data
+                    selectedPicture?.let { selectedPicture->
+                        //We wont convert this image data to bitmap cause Firebase can store image Uri format
+                        binding.imageButton.setImageURI(selectedPicture)
+                    }
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { result ->
+
+            if(result){
+                //permission granted
+                val intentToGallery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncger.launch(intentToGallery)
+            }else{
+                //permission denied
+                Toast.makeText(this@PostActivity,"Permission needed",Toast.LENGTH_LONG).show()
+            }
+
+        }
+        
     }
 }
